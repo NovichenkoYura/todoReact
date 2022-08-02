fba4ab6cacde8834a32d986671c8dc23f68c5b4f
NovichenkoYura <novichenko.y@gmail.com>
2022-08-02 19:27:12 +0300

    structure

--- todoreact/src/components/AddTodo/AddTodo.js
+++ todoreact/src/components/AddTodo/AddTodo.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+import { TodoList } from "../TodoList/TodoList";
+import {v4 as uuidv4} from "uuid"
 
-export const AddTodo = () => {
+export const AddTodo = ({todo, setTodo}) => {
+    const [value, setValue] = useState('')
+    console.log(value)
+
+    const saveTodo = () => {
+        setTodo(
+            [...todo, {
+                id: uuidv4(),
+                title: value,
+                status:true
+                
+            } ]
+        )
+        setValue('')
+    
+}
+
     return (
-        <div>Form</div>
+        <div>
+            <input placeholder="Type task" value={value} onChange={ (e)=>setValue(e.target.value)}/>
+            <button onClick={saveTodo}>Save</button>
+        </div>
     )
 }
--- todoreact/src/components/TodoList/TodoList.js
+++ todoreact/src/components/TodoList/TodoList.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const TodoList = ({ todo, setTodo }) => {
+
+    const [edit, setEdit] = useState(null)
+    const [value, setValue] = useState('')
+  
    
 
     const deleteTodo = (id) => {
@@ -18,13 +22,45 @@ export const TodoList = ({ todo, setTodo }) => {
         })
         setTodo(newTodo)
     }
+
+    const editTodo = (id, title) => {
+        setEdit(id)
+        setValue(title)
+               
+    }
+
+    const saveTodo = () => {
+        
+    }
    
     return (
         <div>{todo.map(item => (
-            <div key = {item.id}>
-                <div>{item.title}</div>
-                <button onClick={() => deleteTodo(item.id)}>Delete</button>
-                <button onClick={() => statusTodo(item.id)}>Close</button>
+            <div key={item.id}>
+                {
+                    edit === item.id ?
+                        <div>
+                            <input value={value} onChange={(e)=>setValue(e.target.value)}
+                            />                            
+                        </div> :
+                        <div>{item.title}</div>                       
+
+                }
+
+                                {
+                    edit === item.id ?
+                        <div>
+                            
+                            <button onClick={ saveTodo}>Save</button>
+                        </div> :
+                        <div>
+                            <button onClick={() => deleteTodo(item.id)}>Delete</button>
+                            <button onClick={() => editTodo(item.id, item.title)}>Edit</button>
+                            <button onClick={() => statusTodo(item.id)}>Close</button>
+                        </div>                       
+
+                }
+                
+
                 
             </div>
         )) }
